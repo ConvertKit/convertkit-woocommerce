58210f0c5f58c00b51f60c3f4e3a3148a7ec53de
Daniel Espinoza <daniel@growdevelopment.com>
2017-08-09 14:31:12 -0500

    Fix #16

--- components/integration/integration.php
+++ components/integration/integration.php
@@ -58,7 +58,7 @@ class CKWC_Integration extends WC_Integration {
 	#region Product Integration
 
 	public function add_meta_boxes($post) {
-		add_meta_box('ckwc', __('Convert Kit Integration'), array($this, 'display_meta_box'), null, $context = 'side', $priority = 'default');
+		add_meta_box('ckwc', __('ConvertKit Integration'), array($this, 'display_meta_box'), null, $context = 'side', $priority = 'default');
 	}
 
 	public function display_meta_box($post) {
