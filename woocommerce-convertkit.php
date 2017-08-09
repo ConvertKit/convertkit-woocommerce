3e1036bb7851fbf6b49297ec37f801f87349167a
Daniel Espinoza <daniel@growdevelopment.com>
2017-08-09 15:26:52 -0500

    Version 1.0.4

--- woocommerce-convertkit.php
+++ woocommerce-convertkit.php
@@ -28,7 +28,7 @@ if ( ! defined( 'CKWC_PLUGIN_FILEPATH' ) ) {
 }
 
 if ( ! defined( 'CKWC_VERSION' ) ) {
-	define( 'CKWC_VERSION', '1.0.1' );
+	define( 'CKWC_VERSION', '1.0.4' );
 }
 
 if ( ! defined( 'CKWC_MIN_WC_VERSION' ) ) {
