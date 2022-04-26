09ce51ca6a9e58b2675ecae8d1e1a68fc0276a61
Tim Carr <tim@n7studios.co.uk>
2022-04-26 13:56:38 +0100

    Settings: Check resource exists before attempting to output dropdown fields

--- views/backend/custom-field-dropdown-field.php
+++ views/backend/custom-field-dropdown-field.php
@@ -14,7 +14,7 @@
 	</option>
 
 	<?php
-	if ( ! is_wp_error( $custom_field['custom_fields']->get() ) ) {
+	if ( $custom_field['custom_fields']->exist() ) {
 		foreach ( $custom_field['custom_fields']->get() as $api_custom_field ) {
 			?>
 			<option value="<?php echo esc_attr( $api_custom_field['key'] ); ?>"<?php selected( esc_attr( $api_custom_field['key'] ), $custom_field['value'] ); ?>><?php echo esc_html( $api_custom_field['label'] ); ?></option>
