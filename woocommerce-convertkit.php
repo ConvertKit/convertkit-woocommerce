659b216f586562f13d82b79dbbd61dd3b9033649
Tim Carr <tim@n7studios.co.uk>
2022-06-13 17:37:54 +0100

    Updated plugin bootstrap to load shared classes if they don’t exist

--- woocommerce-convertkit.php
+++ woocommerce-convertkit.php
@@ -24,12 +24,27 @@ if ( class_exists( 'WP_CKWC' ) ) {
 }
 
 // Define ConverKit Plugin paths and version number.
+define( 'CKWC_PLUGIN_NAME', 'ConvertKitWooCommerce' ); // Used for user-agent in API class.
 define( 'CKWC_PLUGIN_FILE', plugin_basename( __FILE__ ) );
 define( 'CKWC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CKWC_PLUGIN_PATH', __DIR__ );
 define( 'CKWC_PLUGIN_VERSION', '1.4.7' );
 
-// Load files that are always used.
+// Load shared classes, if they have not been included by another ConvertKit Plugin.
+if ( ! class_exists( 'ConvertKit_API' ) ) {
+	require_once CKWC_PLUGIN_PATH . '/lib/class-convertkit-api.php';
+}
+if ( ! class_exists( 'ConvertKit_Log' ) ) {
+	require_once CKWC_PLUGIN_PATH . '/lib/class-convertkit-log.php';
+}
+if ( ! class_exists( 'ConvertKit_Resource' ) ) {
+	require_once CKWC_PLUGIN_PATH . '/lib/class-convertkit-resource.php';
+}
+if ( ! class_exists( 'ConvertKit_Review_Request' ) ) {
+	require_once CKWC_PLUGIN_PATH . '/lib/class-convertkit-review-request.php';
+}
+
+// Load plugin files that are always required.
 require_once CKWC_PLUGIN_PATH . '/includes/functions.php';
 require_once CKWC_PLUGIN_PATH . '/includes/class-wp-ckwc.php';
 require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-api.php';
@@ -40,7 +55,6 @@ require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-resource-custom-fields.php
 require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-resource-forms.php';
 require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-resource-sequences.php';
 require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-resource-tags.php';
-require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-review-request.php';
 require_once CKWC_PLUGIN_PATH . '/includes/class-ckwc-wc-subscriptions.php';
 
 // Load files that are only used in the WordPress Administration interface.
