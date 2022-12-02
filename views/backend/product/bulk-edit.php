696dd0bc1470d762216e15f55efa704f83eac9d9
Tim Carr <tim@n7studios.co.uk>
2022-12-02 15:14:20 +0000

    Improve layout of select and refresh button on bulk and quick edit

--- views/backend/product/bulk-edit.php
+++ views/backend/product/bulk-edit.php
@@ -2,21 +2,23 @@
 /**
  * Bulk Edit view
  *
- * @package ConvertKit
+ * @package CKWC
  * @author ConvertKit
  */
 
 ?>
-<div id="ckwc-bulk-edit" style="display:none;">
+<div id="ckwc-bulk-edit" class="ckwc-bulk-quick-edit">
 	<h4><?php esc_html_e( 'ConvertKit for WooCommerce', 'woocommerce-convertkit' ); ?></h4>
 
-	<?php
-	// Load subscription dropdown field.
-	require CKWC_PLUGIN_PATH . '/views/backend/subscription-dropdown-field.php';
-	?>
-	<button class="ckwc-refresh-resources" class="button button-secondary" title="<?php esc_attr_e( 'Refresh sequences, forms and tags from ConvertKit account', 'woocommerce-convertkit' ); ?>" data-field="#<?php echo esc_attr( $subscription['id'] ); ?>">
-		<span class="dashicons dashicons-update"></span>
-	</button>
+	<div>
+		<?php
+		// Load subscription dropdown field.
+		require CKWC_PLUGIN_PATH . '/views/backend/subscription-dropdown-field.php';
+		?>
+		<button class="ckwc-refresh-resources" class="button button-secondary" title="<?php esc_attr_e( 'Refresh sequences, forms and tags from ConvertKit account', 'woocommerce-convertkit' ); ?>" data-field="#<?php echo esc_attr( $subscription['id'] ); ?>">
+			<span class="dashicons dashicons-update"></span>
+		</button>
+	</div>
 
 	<?php
 	wp_nonce_field( 'ckwc', 'ckwc_nonce' );
