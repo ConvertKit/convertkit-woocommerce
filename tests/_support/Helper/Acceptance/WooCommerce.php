d8a650fa6460763fe926a0f3f1e67194cada6f1d
Tim Carr <tim@n7studios.co.uk>
2023-08-07 14:56:11 +0100

    Tests: Wait for woocommerce_custom_orders_table_enabled visibility

--- tests/_support/Helper/Acceptance/WooCommerce.php
+++ tests/_support/Helper/Acceptance/WooCommerce.php
@@ -78,6 +78,7 @@ class WooCommerce extends \Codeception\Module
 	public function setupWooCommerceHPOS($I)
 	{
 		$I->amOnAdminPage('admin.php?page=wc-settings&tab=advanced&section=features');
+		$I->waitForElementVisible('input[name="woocommerce_custom_orders_table_enabled"]');
 		$I->checkOption('woocommerce_custom_orders_table_enabled');
 		$I->click('Save changes');
 		$I->amOnAdminPage('admin.php?page=wc-settings&tab=advanced&section=custom_data_stores');
