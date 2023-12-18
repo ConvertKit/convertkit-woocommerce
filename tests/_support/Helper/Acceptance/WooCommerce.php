6234434f71671a58bc1e385957f66ce99b8ca29a
Tim Carr <tim@n7studios.co.uk>
2023-12-18 17:07:59 +0000

    Re-attempt tests without wait() method

--- tests/_support/Helper/Acceptance/WooCommerce.php
+++ tests/_support/Helper/Acceptance/WooCommerce.php
@@ -313,8 +313,6 @@ class WooCommerce extends \Codeception\Module
 				// WooCommerce has a bug where clicking the Place Order button the first time doesn't do anything.
 				// This can be reproduced without the ConvertKit for WooCommerce Plugin active, so it's not a conflict.
 				$I->click('button.wc-block-components-checkout-place-order-button');
-				$I->wait(5);
-				$I->click('button.wc-block-components-checkout-place-order-button');
 				break;
 		}
 
