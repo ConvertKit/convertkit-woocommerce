8a9f2c60a5026155041a9a0145d557c2385e73b7
Tim Carr <tim@n7studios.co.uk>
2024-06-05 15:24:09 +0100

    Remove unused helper functions

--- includes/functions.php
+++ includes/functions.php
@@ -30,45 +30,6 @@ function ckwc_get_settings_link( $query_args = array() ) {
 }
 
 /**
- * Helper method to return the URL the user needs to visit to sign in to their ConvertKit account.
- *
- * @since   1.4.2
- *
- * @return  string  ConvertKit Login URL.
- */
-function ckwc_get_sign_in_url() {
-
-	return 'https://app.convertkit.com/?utm_source=wordpress&utm_content=convertkit-for-woocommerce';
-
-}
-
-/**
- * Helper method to return the URL the user needs to visit to sign up for a ConvertKit account.
- *
- * @since   1.4.2
- *
- * @return  string  ConvertKit Signup URL.
- */
-function ckwc_get_signup_url() {
-
-	return 'https://app.convertkit.com/users/signup?utm_source=wordpress&utm_content=convertkit-for-woocommerce';
-
-}
-
-/**
- * Helper method to return the URL the user needs to visit on the ConvertKit app to obtain their API Key and Secret.
- *
- * @since   1.4.2
- *
- * @return  string  ConvertKit App URL.
- */
-function ckwc_get_api_key_url() {
-
-	return 'https://app.convertkit.com/account_settings/advanced_settings/?utm_source=wordpress&utm_content=convertkit-for-woocommerce';
-
-}
-
-/**
  * Helper method to enqueue Select2 scripts for use within the ConvertKit Plugin.
  *
  * @since   1.4.3
