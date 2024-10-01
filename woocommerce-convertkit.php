e844b42c2cd8c1d751a5b403841368576ff658a9
Tim Carr <tim@n7studios.co.uk>
2024-10-01 14:29:03 +0800

    Revert name change

--- woocommerce-convertkit.php
+++ woocommerce-convertkit.php
@@ -6,7 +6,7 @@
  * @author ConvertKit
  *
  * @wordpress-plugin
- * Plugin Name: Kit (formerly ConvertKit) for WooCommerce
+ * Plugin Name: ConvertKit for WooCommerce
  * Plugin URI:  https://www.kit.com
  * Description: Integrates WooCommerce with Kit, allowing customers to be automatically sent to your Kit account.
  * Version: 1.8.6
